8e452fe49066498152f3ff6dcabe219d31c813de
Surajghogare80 <surajghogare1001@gmail.com>
2024-12-04 13:45:13 +0530

    location filter added

---
+++ JavaScript/electronics.js
@@ -0,0 +1,18 @@
+let allimg  = [
+    "./../img/car.png",'./../img/splenderimg-removebg-preview.png','./../img/black-car.png','./../img/black-motorcycle.png',
+];
+let index = 0;
+let images = document.getElementById('new');
+
+
+function next() {
+    if(index<allimg.length-1){
+        index++;
+    }else{
+        index = 0;
+    }
+    images.src = allimg[index]
+}
+setInterval(
+    next,2000
+);
